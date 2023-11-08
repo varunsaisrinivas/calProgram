019b77ebbe9fe1ec38187f8fc60943fe7d006de1
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-11-08 10:05:20 +0530

    First Commit is done.....

---
+++ src/calProgram/Calculator.java
@@ -0,0 +1,41 @@
+package calProgram;
+
+class circle
+{
+	void area1(int a)
+	{
+		
+	}
+}
+class rectangle
+{
+	void area2(int b, int c)
+	{
+		
+	}
+}
+
+class cuboid
+{
+	void area3(int l,int b,int h)
+	{
+		
+	}
+}
+
+
+
+
+
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	
+
+}
