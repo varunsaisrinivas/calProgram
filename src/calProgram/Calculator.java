f8609259986bb530e67470e81daa059e4b07f4c9
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-11-08 10:40:34 +0530

    Test is Done

--- src/calProgram/Calculator.java
+++ src/calProgram/Calculator.java
@@ -4,14 +4,14 @@ class circle
 {
 	void area1(int a)
 	{
-		
+		System.out.println("Area of the circle: " + 3.14 * a *a);
 	}
 }
 class rectangle
 {
 	void area2(int b, int c)
 	{
-		
+		System.out.println("Area of the rectangle: " +  b * c);
 	}
 }
 
@@ -19,7 +19,7 @@ class cuboid
 {
 	void area3(int l,int b,int h)
 	{
-		
+		System.out.println("Area of the cubiod: " + 2*(l*b + b*h + l*h));
 	}
 }
 
@@ -31,7 +31,14 @@ class cuboid
 public class Calculator {
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
+		circle a=new circle();
+		rectangle b=new rectangle();
+		cuboid c= new cuboid();
+		a.area1(10);
+		b.area2(10, 10);
+		c.area3(10, 10, 10);
 
 	}
 	
